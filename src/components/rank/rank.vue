e5037388cb706fb7cf7cf24946358bba125728a5
codinglobster <liuxiaoabc@gmail.com>
2017-08-13 18:38:20 +0800

    top-bar

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="rank">
+    排名
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import fastclick from 'fastclick'
 import router from './router'
+import 'common/stylus/index.styl'
+
+// 解决移动端300毫秒的点击延迟
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
